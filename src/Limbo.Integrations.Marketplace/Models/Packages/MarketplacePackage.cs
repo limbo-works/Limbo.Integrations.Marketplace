09bd6fa16917bcc5dfaf62b65e13daeeb8fc9dd2
Anders Bjerner <abjerner@skybrud.dk>
2023-10-02 19:35:48 +0200

    Added support for more package properties

--- src/Limbo.Integrations.Marketplace/Models/Packages/MarketplacePackage.cs
+++ src/Limbo.Integrations.Marketplace/Models/Packages/MarketplacePackage.cs
@@ -1,12 +1,18 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Limbo.Integrations.Marketplace.Models.Authors;
 using Newtonsoft.Json.Linq;
 using Skybrud.Essentials.Json.Newtonsoft.Extensions;
 using Skybrud.Essentials.Time;
 
+#pragma warning disable CS1591
+
 namespace Limbo.Integrations.Marketplace.Models.Packages;
 
 public class MarketplacePackage : MarketplaceObject {
+
+    public Guid Id { get; }
 
     public string Alias { get; }
 
@@ -20,7 +26,10 @@ public class MarketplacePackage : MarketplaceObject {
     [MemberNotNullWhen(true, "Description")]
     public bool HasDescription => !string.IsNullOrWhiteSpace(Description);
 
-    public string Authors { get; }
+    public string? Authors { get; }
+
+    [MemberNotNullWhen(true, "Authors")]
+    public bool HasAuthors => !string.IsNullOrWhiteSpace(Authors);
 
     public MarketplaceAuthorDetails? AuthorDetails { get; }
 
@@ -34,9 +43,39 @@ public class MarketplacePackage : MarketplaceObject {
     [MemberNotNullWhen(true, "IconUrl")]
     public bool HasIconUrl => !string.IsNullOrWhiteSpace(IconUrl);
 
-    public long Downloads { get; }
+    public long? NumberOfNuGetDownloads { get; }
+
+    [MemberNotNullWhen(true, "NumberOfNuGetDownloads")]
+    public bool HasNumberOfNuGetDownloads => NumberOfNuGetDownloads is not null;
+
+    public string? PackageType { get; }
+
+    public IReadOnlyList<string> LicenseTypes { get; }
+
+    public string? IconDominantColor { get; }
+
+    public bool? IsPromoted { get; }
+
+    public bool? IsPartner { get; }
+
+    public bool? IsHq { get; }
+
+    public bool? IsHqSupported { get; }
+
+    public string? MinimumUmbracoVersionNumber { get; }
+
+    public string? MaximumUmbracoVersionNumber { get; }
+
+    public bool? MaximumUmbracoVersionNumberIsInclusive { get; }
+
+    public string PackageId { get; }
+
+    public IReadOnlyList<string> Tags { get; }
+
+    public IReadOnlyList<int> UmbracoMajorVersionsSupported { get; }
 
     private MarketplacePackage(JObject json) : base(json) {
+        Id = json.GetGuid("id");
         Alias = json.GetString("packageId")!;
         Title = json.GetString("title");
         Description = json.GetString("description");
@@ -44,7 +83,20 @@ public class MarketplacePackage : MarketplaceObject {
         AuthorDetails = json.GetObject("authorDetails", MarketplaceAuthorDetails.Parse);
         LastPublished = json.GetString("lastPublishedOn", EssentialsTime.Parse)!;
         IconUrl = json.GetString("iconUrl");
-        Downloads = json.GetInt64("numberOfNuGetDownloads");
+        NumberOfNuGetDownloads = json.GetInt64OrNull("numberOfNuGetDownloads");
+        PackageType = json.GetString("packageType");
+        LicenseTypes = json.GetStringArray("licenseTypes");
+        IconDominantColor = json.GetString("iconDominantColor");
+        IsPromoted = json.GetBooleanOrNull("isPromoted");
+        IsPartner = json.GetBooleanOrNull("isPartner");
+        IsHq = json.GetBooleanOrNull("isHQ");
+        IsHqSupported = json.GetBooleanOrNull("isHQSupported");
+        MinimumUmbracoVersionNumber = json.GetString("minimumUmbracoVersionNumber");
+        MaximumUmbracoVersionNumber = json.GetString("maximumUmbracoVersionNumber");
+        MaximumUmbracoVersionNumberIsInclusive = json.GetBooleanOrNull("maximumUmbracoVersionNumberIsInclusive");
+        PackageId = json.GetString("packageId")!;
+        Tags = json.GetStringArray("tags");
+        UmbracoMajorVersionsSupported = json.GetInt32Array("umbracoMajorVersionsSupported");
     }
 
     public static MarketplacePackage Parse(JObject parse) {
